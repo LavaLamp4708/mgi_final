0c11b1ac9de897cdfc2f2879260f35cb1d28a236
Yanbo Ma <ma000166@algonquinlive.com>
2024-11-20 00:50:53 -0500

    sales entity fields added

--- lib/database/entities/sales_entity.dart
+++ lib/database/entities/sales_entity.dart
@@ -6,9 +6,13 @@ class SalesEntity {
 
   @primaryKey
   final int id;
-  final String sale;
+  final int customer_id;
+  final int car_id;
+  final int dealership_id;
+  final DateTime date_of_purchase;
 
-  SalesEntity(this.id, this.sale) {
+  SalesEntity(this.id, this.customer_id, this.car_id,
+      this.dealership_id, this.date_of_purchase) {
     ID = id > ID ? id+1 : ID;
   }
 }
