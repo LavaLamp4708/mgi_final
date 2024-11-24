aa1d42104ae11340968bfc7227119ded09afbd70
Yanbo Ma <ma000166@algonquinlive.com>
2024-11-24 08:13:08 -0500

    type changed

--- lib/database/entities/sales_entity.dart
+++ lib/database/entities/sales_entity.dart
@@ -6,10 +6,10 @@ class SalesEntity {
 
   @primaryKey
   final int id;
-  final int customer_id;
-  final int car_id;
-  final int dealership_id;
-  final DateTime date_of_purchase;
+  final String customer_id;
+  final String car_id;
+  final String dealership_id;
+  final String date_of_purchase;
 
   SalesEntity(this.id, this.customer_id, this.car_id,
       this.dealership_id, this.date_of_purchase) {
