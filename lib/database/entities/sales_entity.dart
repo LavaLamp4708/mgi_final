bf2e315804754afd2b21ee502de4e54c2ac449b1
Yanbo Ma <ma000166@algonquinlive.com>
2024-11-28 18:46:49 -0500

    database update

--- lib/database/entities/sales_entity.dart
+++ lib/database/entities/sales_entity.dart
@@ -6,17 +6,17 @@ class SalesEntity {
 
   @primaryKey
   final int id;
-  final String customerId;
-  final String carId;
-  final String dealershipId;
-  final String dateOfPurchase;
+  final String customer_id;
+  final String car_id;
+  final String dealership_id;
+  final String date_of_purchase;
 
   SalesEntity(
       this.id,
-      this.customerId,
-      this.carId,
-      this.dealershipId,
-      this.dateOfPurchase
+      this.customer_id,
+      this.car_id,
+      this.dealership_id,
+      this.date_of_purchase
       ) {
     ID = id > ID ? id+1 : ID;
   }
